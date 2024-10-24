416a8676b8b03ee9135200528a3860e78fcd1339
sh3bh1t <shobhits2000@gmail.com>
2024-10-24 14:56:31 +0530

    major changes in code structure

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+import express from "express";
+import { createServer } from "node:http";
+import dotenv from "dotenv";
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+dotenv.config();
+const uri= process.env.MONGO_URL;
+
+
+app.set("port", (process.env.PORT || 3030))
+app.use(cors({
+    origin: 'https://instacall-1.onrender.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true 
+}));
+
+
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+
+main().then(()=>{
+    console.log("connected to db!");
+}).catch((err)=>{
+    throw err;
+})
+
+async function main(){
+    await mongoose.connect(uri);
+}
+
+
+server.listen(port, ()=>{
+    console.log(`server is listening to port ${port}`);
+})
